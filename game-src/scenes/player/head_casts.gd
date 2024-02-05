78c33af111439fcaa1efc162cec80289a0cce35d
Dewald Els <dewaldifels@gmail.com>
2024-02-05 20:17:16 +0200

    fix: put head collision adjustment to physics loop

--- game-src/scenes/player/head_casts.gd
+++ game-src/scenes/player/head_casts.gd
@@ -10,22 +10,16 @@ extends Node2D
 func _ready() -> void:
 	print("Player", player.name)
 
-func _process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	
 	if not player.is_on_floor():
 		
-		print("left", left_cast.is_colliding())
-		print("center", center_cast.is_colliding())
-		print("right", right_cast.is_colliding())
-		
 		var direction: float = player.scale.y
 		
-		if left_cast.is_colliding() and not right_cast.is_colliding():
-			print("Should push right")
-			player.global_position.x = player.global_position.x + 2 * direction
-		elif right_cast.is_colliding() and not left_cast.is_colliding():
-			player.global_position.x = player.global_position.x - 2 * direction
-			print("Should push left")
+		if left_cast.is_colliding() and not center_cast.is_colliding() and not right_cast.is_colliding():
+			player.global_position.x = player.global_position.x + 3 * direction
+		elif right_cast.is_colliding() and not center_cast.is_colliding() and not left_cast.is_colliding():
+			player.global_position.x = player.global_position.x - 3 * direction
 	
 	
 
